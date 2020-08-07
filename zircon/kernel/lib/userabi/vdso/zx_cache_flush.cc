65d83e0895fb0d766c85d19c108df87b4da26caf
Florent Revest <revest@google.com>
2020-08-07 13:54:47 +0200

    [libzircon] Provide stub code for riscv64
    Change-Id: I8e1f91df2a7445616b8ada2a3956172595338dc2

--- zircon/kernel/lib/userabi/vdso/zx_cache_flush.cc
+++ zircon/kernel/lib/userabi/vdso/zx_cache_flush.cc
@@ -104,6 +104,8 @@ __EXPORT zx_status_t _zx_cache_flush(const void* addr, size_t len, uint32_t opti
     __asm__ volatile("isb sy");
   }
 
+#elif defined(__riscv) && __riscv_xlen == 64
+
 #else
 
 #error what architecture?
